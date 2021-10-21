e2836933e707c2b534294cfd8fe44feec401819b
andreadelucca <projetodigitalam@outlook.com>
2021-10-21 16:18:54 -0400

    Added states functions

--- services/States.py
+++ services/States.py
@@ -6,31 +6,68 @@ import json
 class States(Connection):
 
     def insert(self, data):
+        cursor = self.connection.cursor()
         try:
-            pass
+            sql = "INSERT INTO states VALUES (null, %s, %s)"
+            cursor.execute(sql, data)
+            self.connection.commit()
+            return cursor.rowcount
         except Exception as e:
             return e
 
     def select(self):
+        cursor = self.connection.cursor()
         try:
-            pass
+            cursor.execute("SELECT * FROM states")
+            result = cursor.fetchall()
+            data = dict()
+            for x in result:
+                data.update(
+                    {
+                        x[0]: {
+                            "state": x[1],
+                            "statecode": x[2]
+                        }
+                    }
+                )
+            return json.dumps(data)
         except Exception as e:
             return e
 
     def select_by_id(self, id):
+        cursor = self.connection.cursor()
         try:
-            pass
+            cursor.execute(f"SELECT * FROM states WHERE u.id = {id}")
+            result = cursor.fetchall()
+            data = dict()
+            for x in result:
+                data.update(
+                    {
+                        "id": x[0],
+                        "state": x[1],
+                        "statecode": x[2]
+                    }
+                )
+            return json.dumps(data)
         except Exception as e:
             return e
 
     def update(self, data, clause):
+        cursor = self.connection.cursor()
         try:
-            pass
+            sql = f"UPDATE states SET {data} WHERE id = {clause}"
+            cursor.execute(sql)
+            self.connection.commit()
+            return cursor.rowcount
         except Exception as e:
             return e
 
     def delete(self, id):
+        cursor = self.connection.cursor()
         try:
-            pass
+            sql = f"DELETE FROM states WHERE id = {id}"
+            cursor.execute(sql)
+            self.connection.commit()
+            return cursor.rowcount
         except Exception as e:
             return e
